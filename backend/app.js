30d533d124ec043c4b23eb93eafb850380dd09f1
Mathias BERGER <mathias.codage@gmail.com>
2026-03-04 12:16:54 +0100

    Connexion à MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose'); // <-- IL MANQUAIT CETTE LIGNE !
+require('dotenv').config(); // Décommente ça pour utiliser ton fichier .env
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
+// Utilise la variable d'environnement pour plus de sécurité
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((err) => console.log('Connexion à MongoDB échouée !', err));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-    res.status(201);
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
-app.use((req, res, next) => {
-    res.json({ message: 'Voilà la réponse du serveur !' });
-    next();
+app.post('/api/books', (req, res, next) => {
+  const book = req.body;
+  console.log(book);
+  res.status(201).json({
+    message: 'Books créés avec succès!'
+  });
 });
 
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
+app.get('/api/books', (req, res, next) => {
+    console.log('Récupération des livres');
+  res.status(200).json([]);
 });
 
 module.exports = app;
