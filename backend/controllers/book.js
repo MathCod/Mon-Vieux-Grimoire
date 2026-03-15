1f5d78c81404fa6197e03ffa527e579bc6e490a8
Mathias BERGER <mathias.codage@gmail.com>
2026-03-15 18:06:24 +0100

    Correction codes de status

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -44,13 +44,13 @@ exports.deleteBook = (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then(book => {
       if (book.userId != req.auth.userId) {
-        res.status(401).json({ message : 'Non-authorisé'});
+        res.status(403).json({ message : 'Non-authorisé'});
       } else {
         const filename = book.imageUrl.split('/images/')[1];
         fs.unlink(`images/${filename}`, () => {
           book.deleteOne({ _id: req.params.id })
             .then(() => res.status(200).json({ message : 'Livre supprimé !'}))
-            .catch(error => res.status(401).json({ error }));
+            .catch(error => res.status(500).json({ error }));
         });
       }
     })
